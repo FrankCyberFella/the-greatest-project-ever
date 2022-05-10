25ce9f6f6d7c7e98f12685f07647a659664c923e
Frank Fella <frank.fella@lambdaschool.com>
2022-05-10 11:48:53 -0400

    FFella Push 05/10/2022 11:48:53

--- src/com/amazon/ata/kindlepublishingservice/models/response/RemoveBookFromCatalogResponse.java
+++
@@ -1,6 +0,0 @@
-package com.amazon.ata.kindlepublishingservice.models.response;
-
-public class RemoveBookFromCatalogResponse {
-    public RemoveBookFromCatalogResponse() {
-    }
-}
--- tst/com/amazon/ata/kindlepublishingservice/KindlePublishingClientException.java
+++
@@ -1,5 +0,0 @@
-package com.amazon.ata.kindlepublishingservice;
-
-public class KindlePublishingClientException extends Exception {
-    String message;
-}
